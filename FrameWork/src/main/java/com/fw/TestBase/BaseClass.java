e12f022525e762475c2ee26d07ea72ba10bd7fca
prave <prave@PRAVEEN>
2025-02-18 13:42:55 +0530

    Base Class Added

--- FrameWork/src/main/java/com/fw/TestBase/BaseClass.java
+++ FrameWork/src/main/java/com/fw/TestBase/BaseClass.java
@@ -1,5 +1,31 @@
 package com.fw.TestBase;
 
-public class BaseClass {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class BaseClass {
+	
+	private WebDriver driver;
+
+	
+	public WebDriver createDriver(String browserName)
+	{
+		switch(browserName.toLowerCase())
+		{
+		case "chrome": new ChromeDriver(); break;
+		}
+		return driver;
+	}
+
+
+	public WebDriver getDriver() {
+		return driver;
+	}
+
+
+	public void setDriver(String browserName) {
+		this.driver = createDriver(browserName);
+	}
+	
+	
 }
